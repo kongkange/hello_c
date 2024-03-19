6e74728cb7063ba3b28852d43377b0c412066888
kongkange <kongkange@naver.com>
2024-03-19 16:28:52 +0900

    상품관리 완료 [임동우]

---
+++ hello_c/src/main/java/com/example/demo/GoodsController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodsController {
+
+	@GetMapping("/listgoods")
+	public String listGoods() {
+		return "상품목록";
+	}
+}
